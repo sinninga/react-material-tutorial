b2e48504dc64642166684a0d29c2c49d76c0be61
Adam Sinning <asinning25@gmail.com>
2019-05-01 14:53:37 -0300

    first material ui component created

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
 
 class Header extends Component {
   constructor(props) {
@@ -8,11 +12,17 @@ class Header extends Component {
 
   render() {
     return (
-      <div>
-        heyhey
-      </div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     );
   }
+
 };
 
 export default Header;

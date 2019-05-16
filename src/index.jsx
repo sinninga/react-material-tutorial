c8b879bee3c64660cb3d2608f59d3a7d407f229c
Adam Sinning <asinning25@gmail.com>
2019-05-16 13:39:00 -0300

    inital redux configuration

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 
 import '../assets/stylesheets/application.scss';
 import App from './components/app.jsx';
 
+import playersReducer from './reducers/players_reducer';
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<App name="World" />, root);
-}
+const reducers = combineReducers({
+  players: playersReducer
+});
+
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
